63c31b79eb0d9f1404d09389df636fa85f0ad10b
Roshan Kewat <roshankewat9090@gmail.com>
2024-12-11 18:53:14 +0530

    Made a small change for deployment

--- frontend/src/app/components/NotSupportedPage.jsx
+++ frontend/src/app/components/NotSupportedPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NotSupportedPage = () => {
   return (
     <div className='text-white flex w-full h-[100vh] items-center justify-center px-2'>
-      <span className='text-center'>BaatChit is not available on phones or small size tablets. Download our app</span>
+      <span className='text-center'>AurBhaii is not available on phones or small size tablets. Our App is coming soon.</span>
     </div>
   )
 }
